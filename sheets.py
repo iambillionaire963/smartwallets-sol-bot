dc2f5b591d6e34f191cfa3008af9466df4aaa99d
Stevens Diaz <pineapplebaires@gmail.com>
2025-06-14 17:15:34 +0200

    Your descriptive commit message

--- sheets.py
+++ sheets.py
@@ -1,5 +1,6 @@
 import os
 import json
+import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -10,3 +11,17 @@ if not json_str:
 
 creds_dict = json.loads(json_str)
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
+gc = gspread.authorize(creds)
+
+# Open your sheet by name here
+SPREADSHEET_NAME = "SmartWalletsLog"
+sh = gc.open(SPREADSHEET_NAME)
+worksheet = sh.sheet1  # Use the first worksheet
+
+def log_user(data):
+    """
+    Append a row to the Google Sheet.
+    data: list of values, e.g. ["timestamp", "user_id", "action"]
+    """
+    worksheet.append_row(data)
